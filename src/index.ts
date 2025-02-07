8164f27d5c5136747c86893e8f5ccbd1dd121205
diegozam-dev <128404543+diegozam-dev@users.noreply.github.com>
2025-02-07 16:02:44 -0500

    add index.ts and configuration for server initialization

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,30 @@
-import express from 'express';
+import express, { json } from 'express';
+import cors from 'cors';
+import router from './routes/index';
 import { PORT } from './config';
+import { db } from './db/connection';
 
 const app = express();
+
+app.use(json());
+app.use(cors());
 app.disable('x-powered-by');
 
-app.get('/', (_, res) => {
+app.use(router);
+
+app.get('/', (_req, res) => {
   res.send('Hello World!');
 });
+
+(async () => {
+  await db.batch(
+    [
+      'DROP TABLE IF EXISTS urls',
+      'CREATE TABLE IF NOT EXISTS urls (id INTEGER PRIMARY KEY AUTOINCREMENT, original_url TEXT NOT NULL UNIQUE, code TEXT UNIQUE, short_url TEXT UNIQUE)'
+    ],
+    'write'
+  );
+})();
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);

dabc4679dcdfc67109fad248ac0b719db044da66
diegozam-dev <128404543+diegozam-dev@users.noreply.github.com>
2025-02-10 16:46:06 -0500

    add checkSum validation to shortened urls

--- src/services/url.service.ts
+++ src/services/url.service.ts
@@ -4,20 +4,21 @@ import { BASE_URL } from '../config';
 class UrlService {
   private urlModel: UrlModel;
   private readonly ALPHABET: string;
-  private readonly BASE: number;
+  private readonly ENCODE_BASE: number;
 
   constructor() {
     this.urlModel = new UrlModel();
     this.ALPHABET = '23456789bcdfghjkmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ-_';
-    this.BASE = this.ALPHABET.length;
+    this.ENCODE_BASE = this.ALPHABET.length;
   }
 
   public encodeUrl = async (url: string) => {
     const { lastInsertRowid } = await this.urlModel.create(url);
     const rowId = Number(lastInsertRowid);
 
-    const codeStr = this.generateCodeStr(rowId);
-    const shortUrl = `${BASE_URL}/${codeStr}`;
+    const codeStr = this.getCodeStr(rowId);
+    const checkSumValue = this.getCheckSum(rowId);
+    const shortUrl = `${BASE_URL}/${checkSumValue + codeStr}`;
 
     await this.urlModel.update(rowId, codeStr, shortUrl);
 
@@ -25,25 +26,49 @@ class UrlService {
   };
 
   public decodeUrl = async (codeStr: string) => {
-    let id = 0;
-    for (var i = 0; i < codeStr.length; i++) {
-      id = id * this.BASE + this.ALPHABET.indexOf(codeStr.charAt(i));
-    }
+    if (!this.validateCheckSum(codeStr)) throw new Error('Invalid url.');
+
+    const encodeId = codeStr.substring(2);
+    const id = this.decodeId(encodeId);
 
     return await this.urlModel.getById(id);
   };
 
-  private generateCodeStr(rowId: number) {
+  private decodeId = (encodedId: string) => {
+    let id = 0;
+    for (var i = 0; i < encodedId.length; i++) {
+      id = id * this.ENCODE_BASE + this.ALPHABET.indexOf(encodedId.charAt(i));
+    }
+
+    return id;
+  };
+
+  private getCodeStr = (rowId: number) => {
     let id = rowId;
     let codeStr = '';
 
     while (id > 0) {
-      codeStr = this.ALPHABET.charAt(id % this.BASE) + codeStr;
-      id = Math.floor(id / this.BASE);
+      codeStr = this.ALPHABET.charAt(id % this.ENCODE_BASE) + codeStr;
+      id = Math.floor(id / this.ENCODE_BASE);
     }
 
     return codeStr;
-  }
+  };
+
+  private getCheckSum = (id: number) => {
+    const checkSumValue =
+      (id * 17 + 31) % (this.ENCODE_BASE * this.ENCODE_BASE);
+
+    return this.getCodeStr(checkSumValue).padStart(2, this.ALPHABET.charAt(0));
+  };
+
+  private validateCheckSum = (codeStr: string) => {
+    const checkSumValue = codeStr.substring(0, 2);
+    const encodedId = codeStr.substring(2);
+    const id = this.decodeId(encodedId);
+
+    return this.getCheckSum(id) === checkSumValue;
+  };
 }
 
 export default UrlService;

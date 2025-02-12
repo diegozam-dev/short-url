6fb6d45a502aa762457785447e5b5f6b301050ac
diegozam-dev <128404543+diegozam-dev@users.noreply.github.com>
2025-02-12 16:37:55 -0500

    add function to validate urls

---
+++ src/utils/validateUrl.ts
@@ -0,0 +1,14 @@
+export const isValidUrl = (url: string) => {
+  const urlPattern = new RegExp(
+    '^(https?:\\/\\/)?' +
+      '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' +
+      '((\\d{1,3}\\.){3}\\d{1,3})|' +
+      '(localhost))' +
+      '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' +
+      '(\\?[;&a-z\\d%_.~+=-]*)?' +
+      '(\\#[-a-z\\d_]*)?$',
+    'i'
+  );
+
+  return !!urlPattern.test(url);
+};

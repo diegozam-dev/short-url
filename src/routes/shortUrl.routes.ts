1e633a1cb912acc3a3cfd26c28e087d8d5e5c4a3
diegozam-dev <128404543+diegozam-dev@users.noreply.github.com>
2025-02-13 16:53:39 -0500

    add validateUrl and scanUrl middlewares for the encode endpoint

--- src/routes/shortUrl.routes.ts
+++ src/routes/shortUrl.routes.ts
@@ -1,10 +1,17 @@
 import { Router } from 'express';
 import ShortUrlController from '../controllers/shortUrl.controller';
+import scanUrl from '../middlewares/scanUrl.middleware';
+import validateUrl from '../middlewares/validateUrl.middleware';
 
 const shortUrlController = new ShortUrlController();
 const shortUrlRouter = Router();
 
-shortUrlRouter.post('/encode', shortUrlController.createShortUrl);
+shortUrlRouter.post(
+  '/encode',
+  validateUrl,
+  scanUrl,
+  shortUrlController.createShortUrl
+);
 shortUrlRouter.get('/decode/:checkSumId', shortUrlController.getOriginalUrl);
 shortUrlRouter.get(
   '/redirect/:checkSumId',

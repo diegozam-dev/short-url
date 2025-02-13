d4eccb6a80b56ffad9a2d217deba83d303ecde23
diegozam-dev <128404543+diegozam-dev@users.noreply.github.com>
2025-02-13 13:15:32 -0500

    Add some comments and move middleware to the bottom

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,21 +1,27 @@
 import { Request, Response, Router } from 'express';
 import shortUrlRouter from './shortUrl.routes';
 import routeNotFound from '../middlewares/routeNotFound.middleware';
-import { BASE_URL } from '../config';
 import errorHandler from '../middlewares/errorHandler.middleware';
+import { BASE_URL } from '../config';
 
 const apiRouter = Router();
 const router = Router();
 
 apiRouter.use('/url', shortUrlRouter);
 
-apiRouter.use(routeNotFound);
-apiRouter.use(errorHandler);
-
 router.use('/v1/api', apiRouter);
-router.use('/:checkSumId', (req: Request, res: Response) => {
+
+router.get('/', (_req, res) => {
+  res.send('Hello World!');
+});
+
+router.get('/:checkSumId', (req: Request, res: Response) => {
   const { checkSumId } = req.params;
   res.redirect(301, `${BASE_URL}/v1/api/url/redirect/${checkSumId}`);
 });
+
+// Middlewares
+router.use(routeNotFound);
+router.use(errorHandler);
 
 export default router;

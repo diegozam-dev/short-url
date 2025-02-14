b5b8f4a8f5bc2ad40e04eca5c3943a62f23c1735
diegozam-dev <128404543+diegozam-dev@users.noreply.github.com>
2025-02-14 14:49:20 -0500

    update ErrorEnum import and add url pattern

--- src/middlewares/validateUrl.middleware.ts
+++ src/middlewares/validateUrl.middleware.ts
@@ -1,11 +1,19 @@
 import { NextFunction, Request, Response } from 'express';
-import urlPattern from '../utils/urlPattern';
-import ErrorEnum from '../utils/errorEnum';
+import ErrorEnum from '../errors/errorEnum';
 
 const validateUrl = (req: Request, res: Response, next: NextFunction) => {
   const { url } = req.body;
 
-  const urlRegExp = new RegExp(urlPattern, 'i');
+  const urlRegExp = new RegExp(
+    '^(https?:\\/\\/)?' +
+      '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' +
+      '((\\d{1,3}\\.){3}\\d{1,3})|' +
+      '(localhost))' +
+      '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' +
+      '(\\?[;&a-z\\d%_.~+=-]*)?' +
+      '(\\#[-a-z\\d_]*)?$',
+    'i'
+  );
   const validateResult = !!urlRegExp.test(url);
 
   if (!validateResult) {

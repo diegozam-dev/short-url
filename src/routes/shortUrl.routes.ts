d9d4ef9554f1365f1cad569a9b2327be0f8fdf4a
diegozam-dev <128404543+diegozam-dev@users.noreply.github.com>
2025-02-14 17:27:00 -0500

    add cache to routes

--- src/routes/shortUrl.routes.ts
+++ src/routes/shortUrl.routes.ts
@@ -2,12 +2,25 @@ import { Router } from 'express';
 import ShortUrlController from '../controllers/shortUrl.controller';
 import scanUrl from '../middlewares/scanUrl.middleware';
 import validateUrl from '../middlewares/validateUrl.middleware';
+import rateLimiter from '../middlewares/rateLimiter.middleware';
+import apicache from 'apicache';
 
 const shortUrlController = new ShortUrlController();
 const shortUrlRouter = Router();
 
+const cache = apicache.options({
+  defaultDuration: '10 minute',
+  statusCodes: {
+    include: [200]
+  }
+}).middleware;
+
+// Middlewares
+shortUrlRouter.use(cache());
+
 shortUrlRouter.post(
   '/encode',
+  rateLimiter,
   validateUrl,
   scanUrl,
   shortUrlController.createShortUrl
